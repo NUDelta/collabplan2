8b3ce8c918f31bccc6a91ae554e3ae5bce56212f
[katiegeorge] <[katiegeorge@u.northwestern.edu]>
2016-04-06 12:01:54 -0500

    added js helpers

--- client/views/LearnerHome/LearnerHome.js
+++ client/views/LearnerHome/LearnerHome.js
@@ -1,4 +1,10 @@
 Template['LearnerHome'].helpers({
+	user_pending_action_plans: function () {
+        return ActionPlans.find({isComplete: false, requester_id: Meteor.userId()}, {sort: { name: 1 }})
+    },
+    user_complete_action_plans: function () {
+        return ActionPlans.find({isComplete: true, requester_id: Meteor.userId()}, {sort: { name: 1 }})
+    },
 });
 
 Template['LearnerHome'].events({
