11365e1ccfc52538e6cb927aaa0ee081480967cd
[katiegeorge] <[katiegeorge@u.northwestern.edu]>
2016-04-07 18:12:51 -0500

    added table with working status

--- client/views/LearnerHome/LearnerHome.js
+++ client/views/LearnerHome/LearnerHome.js
@@ -5,6 +5,27 @@ Template['LearnerHome'].helpers({
     user_complete_action_plans: function () {
         return ActionPlans.find({isComplete: true, requester_id: Meteor.userId()}, {sort: { name: 1 }})
     },
+
+    settings: function () {
+        return {
+            collection: ActionPlans,
+            rowsPerPage: 10,
+            showFilter: true,
+            fields: [
+            	{ key:'name', label: 'Name' },
+            	{ 
+            		key:'isComplete', 
+            		label: 'Status',
+            		fn: function (value) { 
+            			if (value) {return 'Ready'}
+            			else {return 'Pending'}; 
+            		}
+
+            	},
+            	{ key:'submit date', label: 'Submit Date' }
+            ]
+        };
+    }
 });
 
 Template['LearnerHome'].events({
