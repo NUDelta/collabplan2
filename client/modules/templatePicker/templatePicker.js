1cfe9e0b5a1f337245e58468224b67cbc4d787b5
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-04-20 12:26:54 -0500

    panel module for selecting templates

---
+++ client/modules/templatePicker/templatePicker.js
@@ -0,0 +1,35 @@
+Template['templatePicker'].helpers({
+    templates: function () {
+        return [
+            {
+                name: "Test Action Plan",
+                description: "An action plan for testing",
+                isComplete: false,
+                author_id: 'admin',
+                requester_id: 'test',
+                milestone_ids: [
+                  {
+                    title: "Test Boilerplate",
+                    motivation: "Get shit started",
+                    tags: ['python'],
+                    subtask_ids: [
+                      {
+                        description: "Set up site",
+                        milestone_ids: [],
+                        links: ['test.com']
+                      },
+                      {
+                        description: "More set up stuff",
+                        milestone_ids: [],
+                        links: ['test.com']
+                      },
+                    ],
+                  },
+                ],
+              }
+        ]
+    }
+});
+
+Template['templatePicker'].events({
+});
