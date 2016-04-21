bc64c9a5086b0a55d768ee7c761135ce52803e62
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-04-21 16:55:42 -0500

    finished entering sample template

--- client/modules/templatePicker/templatePicker.js
+++ client/modules/templatePicker/templatePicker.js
@@ -116,6 +116,102 @@ Template['templatePicker'].helpers({
                       }
                     ],
                   },
+                  {
+                    title: "Add account library",
+                    motivation: "The site should only allowed logged in users to create items. Add an account library for framework to your webapp. This will handle the account management and authentication flow.",
+                    tags: [],
+                    subtask_ids: [
+                      {
+                        description: "Add library for handling user accounts",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add login buttons to your site",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add user id attribute to item’s schema",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Have user id set on item form submit to the logged in user's id",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Only allow logged in users to access to item creation page",
+                        milestone_ids: [],
+                        links: []
+                      }
+                    ],
+                  },
+                  {
+                    title: "Create a user profile page that list all the user’s items",
+                    motivation: "Users need to be able to view specific author profiles and thier items. This page will be a template to show any user based on route. The route will specify with user to show.",
+                    tags: [],
+                    subtask_ids: [
+                      {
+                        description: "Create template and controller files",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add route to show page based item id, like /user/:id",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Query single user from database using route id",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add content from database to template",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Query all items created by user from database using route id",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Display all items by creating a loop in the template to render the item content",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Edit all items view to have each item show thier author and link to their respective author view",
+                        milestone_ids: [],
+                        links: []
+                      }
+                    ],
+                  },
+                  {
+                    title: "Add delete item buttons to user profile page if its for the logged in user",
+                    motivation: "Users should be allowed to delete items they have made. When a user views their profile, they will be show delete buttons for each item.",
+                    tags: [],
+                    subtask_ids: [
+                      {
+                        description: "Create a method to confirm user is the owner of the current profile page",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add a delete button to the template loop if the previous method is true",
+                        milestone_ids: [],
+                        links: []
+                      },
+                      {
+                        description: "Add a click handler for the button that will delete the item from the database",
+                        milestone_ids: [],
+                        links: []
+                      }
+                    ],
+                  },
                 ],
             }
         ]
