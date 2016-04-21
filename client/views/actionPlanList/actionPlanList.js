7b8dc2feed67c42d851106ccb7dfd93c0711f170
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-04-21 16:06:54 -0500

    prod fix

--- client/views/actionPlanList/actionPlanList.js
+++ client/views/actionPlanList/actionPlanList.js
@@ -1,17 +1,17 @@
-Template['actionPlanList'].helpers({
-    user_pending_action_plans: function () {
-        return ActionPlans.find({isComplete: false, requester_id: Meteor.userId()}, {sort: { name: 1 }})
-    },
-    user_complete_action_plans: function () {
-        return ActionPlans.find({isComplete: true, requester_id: Meteor.userId()}, {sort: { name: 1 }})
-    },
-    other_pending_action_plans: function () {
-        return ActionPlans.find({isComplete: false, requester_id: {$not: Meteor.userId()}, author_id: { $exists: false }}, {sort: { name: 1 }})
-    },
-    user_working_on_action_plans: function () {
-        return ActionPlans.find({isComplete: false, requester_id: {$not: Meteor.userId()}, author_id: Meteor.userId()}, {sort: { name: 1 }})
-    }
-});
+// Template['actionPlanList'].helpers({
+//     user_pending_action_plans: function () {
+//         return ActionPlans.find({isComplete: false, requester_id: Meteor.userId()}, {sort: { name: 1 }})
+//     },
+//     user_complete_action_plans: function () {
+//         return ActionPlans.find({isComplete: true, requester_id: Meteor.userId()}, {sort: { name: 1 }})
+//     },
+//     other_pending_action_plans: function () {
+//         return ActionPlans.find({isComplete: false, requester_id: {$not: Meteor.userId()}, author_id: { $exists: false }}, {sort: { name: 1 }})
+//     },
+//     user_working_on_action_plans: function () {
+//         return ActionPlans.find({isComplete: false, requester_id: {$not: Meteor.userId()}, author_id: Meteor.userId()}, {sort: { name: 1 }})
+//     }
+// });
 
-Template['actionPlanList'].events({
-});
+// Template['actionPlanList'].events({
+// });
