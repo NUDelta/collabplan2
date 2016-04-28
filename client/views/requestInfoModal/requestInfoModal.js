7974970bdc6ac3f07794e251e20d4b86cd1195e6
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-04-28 15:28:00 -0500

    bug with user's skills

--- client/views/requestInfoModal/requestInfoModal.js
+++ client/views/requestInfoModal/requestInfoModal.js
@@ -1,4 +1,7 @@
 Template['requestInfoModal'].helpers({
+    request_user_skills: function (user_id) {
+        Meteor.users.findOne({_id: user_id}).profile.skills;
+    }
 });
 
 Template['requestInfoModal'].events({
