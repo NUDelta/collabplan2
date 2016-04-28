2dca7b396664a693e89c7d89028b7bb1f2c3e702
Nikhil Pai <nikhilpai2016@u.northwestern.edu>
2016-04-28 15:34:40 -0500

    stupid bugs from moving too fast:

--- client/views/requestInfo/requestInfo.js
+++ client/views/requestInfo/requestInfo.js
@@ -1,4 +1,7 @@
 Template['requestInfo'].helpers({
+    request_user_skills: function (user_id) {
+        return Meteor.users.findOne({_id: user_id}).profile.skills;
+    }
 });
 
 Template['requestInfo'].events({
--- client/views/requestInfoModal/requestInfoModal.js
+++ client/views/requestInfoModal/requestInfoModal.js
@@ -1,7 +1,5 @@
 Template['requestInfoModal'].helpers({
-    request_user_skills: function (user_id) {
-        Meteor.users.findOne({_id: user_id}).profile.skills;
-    }
+    
 });
 
 Template['requestInfoModal'].events({
